9e4ac01d734cecc3fe27d9caea0e743f1f20f21f
alwaninajiah <alwani.najaih@gmail.com>
2025-01-07 17:13:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Delay for 3 seconds (3000ms), then redirect to the homepage
+setTimeout(function() {
+    window.location.href = "test.html"; // Replace with your actual homepage URL
+}, 3000); // Adjust the time to match the animation duration
